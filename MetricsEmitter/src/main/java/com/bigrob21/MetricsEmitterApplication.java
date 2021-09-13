74fc88b5e6bdb4034f39d33e2ae6b4b3ca67a7f9
bigrob21 <23691570+bigrob21@users.noreply.github.com>
2021-09-12 20:59:55 -0400

    Adding custom health indicator for actuator. And re-added missing property.

--- MetricsEmitter/src/main/java/com/bigrob21/MetricsEmitterApplication.java
+++ MetricsEmitter/src/main/java/com/bigrob21/MetricsEmitterApplication.java
@@ -2,7 +2,12 @@ package com.bigrob21;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
 
+@ComponentScans({
+	@ComponentScan(basePackages = "custom.health")
+})
 @SpringBootApplication
 public class MetricsEmitterApplication {
 
